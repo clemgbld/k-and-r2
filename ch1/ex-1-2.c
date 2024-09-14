49c46089795ee5478b01f34efdef1862d2303b57
cgombauld <clement.gombauld@cpexterne.org>
2024-09-14 16:28:11 +0200

    solve ex 2 of chapter 1

---
+++ ch1/ex-1-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() { printf("what happen when with that char ? \c \n"); }
+
+// will print "what happen when with that char ? c ".
+// which means when you escape a character that you souldn't it is transformed
+// to the unescape character.
