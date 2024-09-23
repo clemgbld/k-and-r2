8c63e05983be9ef85b1b1e694a40582bbb56a7ef
cgombauld <clement.gombauld@cpexterne.org>
2024-09-23 09:23:40 +0200

    solve ex 21 of chapter 1

---
+++ ch1/ex-1-21.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+  int c;
+  int i = 0;
+  const int TAB_SPACES = 6;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      int numberOfSpaces = 1;
+      while ((c = getchar()) == ' ') {
+        numberOfSpaces++;
+      }
+      int spaceToAdd = TAB_SPACES - (i % TAB_SPACES);
+      while (spaceToAdd != 0 && spaceToAdd <= numberOfSpaces) {
+        putchar('\t');
+        i += spaceToAdd;
+        numberOfSpaces -= spaceToAdd;
+        spaceToAdd = TAB_SPACES - (i % TAB_SPACES);
+      }
+
+      while (numberOfSpaces > 0) {
+        putchar(' ');
+        i++;
+        numberOfSpaces--;
+      }
+      putchar(c);
+    } else if (c == '\n') {
+      i = 0;
+      putchar(c);
+    } else {
+      putchar(c);
+      i++;
+    }
+  }
+  return 0;
+}
