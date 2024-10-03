3d7b07f5a6592d4251a8fb61a9ffe430a4f29bd1
cgombauld <clement.gombauld@cpexterne.org>
2024-10-03 09:01:09 +0200

    continued ex 24 of chapter 1

--- ch1/ex-1-24.c
+++ ch1/ex-1-24.c
@@ -1,4 +1,18 @@
 void countSyntaxes(char c, int syntaxesCount[], char modes[]) {
+
+  if (c == '/') {
+    modes[0] = '/';
+  }
+
+  if (c == '*') {
+    if (modes[1] == '*') {
+      modes[2] = '*';
+    } else {
+      syntaxesCount[10] = syntaxesCount[10] + 1;
+      modes[1] = '*';
+    }
+  }
+
   if (c == '(') {
     syntaxesCount[0] = syntaxesCount[0] + 1;
   }
@@ -23,7 +37,22 @@ void countSyntaxes(char c, int syntaxesCount[], char modes[]) {
   }
 
   if (c == '"') {
-    syntaxesCount[6] = syntaxesCount[6] + 1;
-    modes[0] = '"';
+    if (modes[0] == '"') {
+      syntaxesCount[7] = syntaxesCount[7] + 1;
+      modes[0] = 0;
+    } else {
+      syntaxesCount[6] = syntaxesCount[6] + 1;
+      modes[0] = '"';
+    }
+  }
+
+  if (c == '\'') {
+    if (modes[0] == '\'') {
+      syntaxesCount[9] = syntaxesCount[9] + 1;
+      modes[0] = 0;
+    } else {
+      syntaxesCount[8] = syntaxesCount[8] + 1;
+      modes[0] = '\'';
+    }
   }
 };
--- ch1/tests/ex-1-24.spec.c
+++ ch1/tests/ex-1-24.spec.c
@@ -123,6 +123,110 @@ static MunitResult shouldCountOpenDoubleQuote(const MunitParameter params[],
   return MUNIT_OK;
 }
 
+static MunitResult shouldCountCloseDoubleQuotes(const MunitParameter params[],
+                                                void *fixture) {
+  int initial = atoi(munit_parameters_get(params, "initial"));
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, initial, 0, 0, 0, 0};
+  char modes[] = {'"', 0, 0, 0};
+
+  countSyntaxes('"', syntaxesCount, modes);
+
+  assert_arrays_int_equal(
+      syntaxesCount, (int[]){0, 0, 0, 0, 0, 0, 0, initial + 1, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){0, 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldCountOpenSingleQuotes(const MunitParameter params[],
+                                               void *fixture) {
+  int initial = atoi(munit_parameters_get(params, "initial"));
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, initial, 0, 0, 0};
+  char modes[] = {0, 0, 0, 0};
+
+  countSyntaxes('\'', syntaxesCount, modes);
+
+  assert_arrays_int_equal(
+      syntaxesCount, (int[]){0, 0, 0, 0, 0, 0, 0, 0, initial + 1, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'\'', 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldCountCloseSingleQuotes(const MunitParameter params[],
+                                                void *fixture) {
+  int initial = atoi(munit_parameters_get(params, "initial"));
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, initial, 0, 0};
+  char modes[] = {'\'', 0, 0, 0};
+
+  countSyntaxes('\'', syntaxesCount, modes);
+
+  assert_arrays_int_equal(
+      syntaxesCount, (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, initial + 1, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){0, 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldInitializeCommentMode(const MunitParameter params[],
+                                               void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {0, 0, 0, 0};
+
+  countSyntaxes('/', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'/', 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldCountOpenComments(const MunitParameter params[],
+                                           void *fixture) {
+  int initial = atoi(munit_parameters_get(params, "initial"));
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, initial, 0};
+  char modes[] = {'/', 0, 0, 0};
+
+  countSyntaxes('*', syntaxesCount, modes);
+
+  assert_arrays_int_equal(
+      syntaxesCount, (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, initial + 1, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'/', '*', 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldInitializeCloseComment(const MunitParameter params[],
+                                                void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {'/', '*', 0, 0};
+
+  countSyntaxes('*', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'/', '*', '*', 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldCountCloseComments(const MunitParameter params[],
+                                            void *fixture) {
+  int initial = atoi(munit_parameters_get(params, "initial"));
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, initial};
+  char modes[] = {'/', '*', '*', 0};
+
+  countSyntaxes('/', syntaxesCount, modes);
+
+  assert_arrays_int_equal(
+      syntaxesCount, (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 00, initial + 1},
+      12);
+  assert_arrays_char_equal(modes, (char[]){0, 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
 MunitTest tests[] = {{
                          "should count open parenthesis", /* name */
                          shouldCountOpenParenthesis,      /* test */
@@ -178,6 +282,62 @@ MunitTest tests[] = {{
                          NULL,                              /* tear_down */
                          MUNIT_TEST_OPTION_NONE,            /* options */
                          test_params                        /* parameters */
+                     },
+                     {
+                         "should count close doubles quotes", /* name */
+                         shouldCountCloseDoubleQuotes,        /* test */
+                         NULL,                                /* setup */
+                         NULL,                                /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,              /* options */
+                         test_params                          /* parameters */
+                     },
+                     {
+                         "should count open single quotes", /* name */
+                         shouldCountOpenSingleQuotes,       /* test */
+                         NULL,                              /* setup */
+                         NULL,                              /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,            /* options */
+                         test_params                        /* parameters */
+                     },
+                     {
+                         "should count close single quotes", /* name */
+                         shouldCountCloseSingleQuotes,       /* test */
+                         NULL,                               /* setup */
+                         NULL,                               /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,             /* options */
+                         test_params                         /* parameters */
+                     },
+                     {
+                         "should initialize comment mode", /* name */
+                         shouldInitializeCommentMode,      /* test */
+                         NULL,                             /* setup */
+                         NULL,                             /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,           /* options */
+                         NULL                              /* parameters */
+                     },
+                     {
+                         "should count open comments", /* name */
+                         shouldCountOpenComments,      /* test */
+                         NULL,                         /* setup */
+                         NULL,                         /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,       /* options */
+                         test_params                   /* parameters */
+                     },
+                     {
+                         "should initialize close comments", /* name */
+                         shouldInitializeCloseComment,       /* test */
+                         NULL,                               /* setup */
+                         NULL,                               /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,             /* options */
+                         NULL                                /* parameters */
+                     },
+                     {
+                         "should count close comments", /* name */
+                         shouldCountCloseComments,      /* test */
+                         NULL,                          /* setup */
+                         NULL,                          /* tear_down */
+                         MUNIT_TEST_OPTION_NONE,        /* options */
+                         test_params                    /* parameters */
                      },
                      /* Mark the end of the array with an entry where the test
                       * function is NULL */
