c72e20323bd3a1f10504c0a4fcda5ee9d4b7c27f
cgombauld <clement.gombauld@cpexterne.org>
2024-10-05 17:04:00 +0200

    solve ex 24 of chapter 1

--- ch1/ex-1-24.c
+++ ch1/ex-1-24.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
+
 int isInLineComments(char modes[]) {
   return modes[0] == '/' && modes[1] == '/';
 }
 
+int isInMultiComments(char modes[]) {
+  return modes[0] == '/' && modes[1] == '*';
+}
+
 void countSyntaxes(char c, int syntaxesCount[], char modes[]) {
 
-  if (c == '/') {
+  if (c == '/' && modes[0] != '"' && modes[0] != '\'') {
     if (modes[0] == '/' && modes[2] == '*') {
       syntaxesCount[11] = syntaxesCount[11] + 1;
       modes[0] = 0;
@@ -21,39 +27,46 @@ void countSyntaxes(char c, int syntaxesCount[], char modes[]) {
     modes[2] = 0;
   }
 
-  if (c == '*') {
+  if (c == '*' && !isInLineComments(modes)) {
     if (modes[1] == '*') {
       modes[2] = '*';
-    } else {
+    } else if (modes[0] == '/') {
       syntaxesCount[10] = syntaxesCount[10] + 1;
       modes[1] = '*';
     }
   }
 
-  if (c == '(') {
+  if (c == '(' && modes[0] != '"' && modes[0] != '\'' &&
+      !isInMultiComments(modes) && !isInLineComments(modes)) {
     syntaxesCount[0] = syntaxesCount[0] + 1;
   }
-  if (c == ')') {
+  if (c == ')' && modes[0] != '"' && modes[0] != '\'' &&
+      !isInMultiComments(modes) && !isInLineComments(modes)) {
     syntaxesCount[1] = syntaxesCount[1] + 1;
   }
 
-  if (c == '{' && modes[0] != '"') {
+  if (c == '{' && modes[0] != '"' && modes[0] != '\'' &&
+      !isInMultiComments(modes) && !isInLineComments(modes)) {
     syntaxesCount[2] = syntaxesCount[2] + 1;
   }
 
-  if (c == '}') {
+  if (c == '}' && modes[0] != '"' && modes[0] != '\'' &&
+      !isInMultiComments(modes) && !isInLineComments(modes)) {
     syntaxesCount[3] = syntaxesCount[3] + 1;
   }
 
-  if (c == '[') {
+  if (c == '[' && modes[0] != '"' && modes[0] != '\'' &&
+      !isInMultiComments(modes) && !isInLineComments(modes)) {
     syntaxesCount[4] = syntaxesCount[4] + 1;
   }
 
-  if (c == ']') {
+  if (c == ']' && modes[0] != '"' && modes[0] != '\'' &&
+      !isInMultiComments(modes) && !isInLineComments(modes)) {
     syntaxesCount[5] = syntaxesCount[5] + 1;
   }
 
-  if (c == '"') {
+  if (c == '"' && modes[0] != '\'' && !isInMultiComments(modes) &&
+      !isInLineComments(modes) && modes[3] != '\\') {
     if (modes[0] == '"') {
       syntaxesCount[7] = syntaxesCount[7] + 1;
       modes[0] = 0;
@@ -63,7 +76,8 @@ void countSyntaxes(char c, int syntaxesCount[], char modes[]) {
     }
   }
 
-  if (c == '\'') {
+  if (c == '\'' && modes[0] != '"' && !isInMultiComments(modes) &&
+      !isInLineComments(modes) && modes[3] != '\\') {
     if (modes[0] == '\'') {
       syntaxesCount[9] = syntaxesCount[9] + 1;
       modes[0] = 0;
@@ -77,4 +91,38 @@ void countSyntaxes(char c, int syntaxesCount[], char modes[]) {
     modes[0] = 0;
     modes[1] = 0;
   }
+
+  if (modes[3] == '\\') {
+    modes[3] = 0;
+  }
+
+  if (c == '\\') {
+    modes[3] = '\\';
+  }
 };
+
+int main() {
+  int syntaxesCount[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[4] = {0, 0, 0, 0};
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    countSyntaxes(c, syntaxesCount, modes);
+  }
+
+  char *messages[12] = {" parenthesis ",      " parenthesis ",
+                        " curly brackets ",   " curly brackets ",
+                        " squared brackets ", " squared brackets ",
+                        " double quotes ",    " double quotes ",
+
+                        " single quotes ",    " single quotes ",
+                        " comments ",         " comments "};
+
+  for (int i = 0; i < 12; i += 2) {
+    if (syntaxesCount[i] != syntaxesCount[i + 1]) {
+      printf("%s%s%s", "Unbalenced", messages[i], "at the end of the file\n");
+    }
+  }
+
+  return 0;
+}
--- ch1/tests/ex-1-24.spec.c
+++ ch1/tests/ex-1-24.spec.c
@@ -300,17 +300,167 @@ shouldOnlyEndLineCommentModeWhenInCommentLine(const MunitParameter params[],
   return MUNIT_OK;
 }
 
+static char *initial_params_double_quote[] = {"(", ")", "{", "}", "[",
+                                              "]", "/", "'", NULL};
+
+static MunitParameterEnum test_params_double_quotes[] = {
+    {"params", initial_params_double_quote}, {NULL, NULL}};
+
 static MunitResult
 shouldNotCountCharWhenInDoubleQuote(const MunitParameter params[],
                                     void *fixture) {
   int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
   char modes[] = {'"', 0, 0, 0};
 
-  countSyntaxes('{', syntaxesCount, modes);
+  const char *characters = munit_parameters_get(params, "params");
+
+  countSyntaxes(characters[0], syntaxesCount, modes);
 
   assert_arrays_int_equal(syntaxesCount,
                           (int[]){0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, 12);
   assert_arrays_char_equal(modes, (char[]){'"', 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
+shouldNotTryToInitializeMultiCommentModeWhenThereIsNoSlashPreviously(
+    const MunitParameter params[], void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {0, 0, 0, 0};
+
+  countSyntaxes('*', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){0, 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static char *initial_params_single_quote[] = {"(", ")", "{",  "}", "[",
+                                              "]", "/", "\"", NULL};
+
+static MunitParameterEnum test_params_single_quotes[] = {
+    {"params", initial_params_single_quote}, {NULL, NULL}};
+
+static MunitResult
+shouldNotCountCharWhenInSingleQuote(const MunitParameter params[],
+                                    void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+  char modes[] = {'\'', 0, 0, 0};
+
+  const char *characters = munit_parameters_get(params, "params");
+
+  countSyntaxes(characters[0], syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'\'', 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static char *initial_params_multi_comments_quote[] = {"(", ")", "{",  "}", "[",
+                                                      "]", "'", "\"", NULL};
+
+static MunitParameterEnum test_params_multi_comments_quotes[] = {
+    {"params", initial_params_multi_comments_quote}, {NULL, NULL}};
+
+static MunitResult
+shouldNotTryToCountCharWhenInMultipleComments(const MunitParameter params[],
+                                              void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0};
+  char modes[] = {'/', '*', 0, 0};
+
+  const char *characters = munit_parameters_get(params, "params");
+
+  countSyntaxes(characters[0], syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'/', '*', 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static char *initial_params_line_comments_quote[] = {"(", ")", "{",  "}", "[",
+                                                     "]", "'", "\"", "*", NULL};
+
+static MunitParameterEnum test_params_line_comments_quotes[] = {
+    {"params", initial_params_line_comments_quote}, {NULL, NULL}};
+
+static MunitResult
+shouldNotTryToCountCharWhenInLineComment(const MunitParameter params[],
+                                         void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {'/', '/', 0, 0};
+
+  const char *characters = munit_parameters_get(params, "params");
+
+  countSyntaxes(characters[0], syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'/', '/', 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldBeAbleToPassInEscapeMode(const MunitParameter params[],
+                                                  void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {'"', 0, 0, 0};
+
+  countSyntaxes('\\', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'"', 0, 0, '\\'}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult shouldBeAbleToExitEscapeMode(const MunitParameter params[],
+                                                void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {'"', 0, 0, '\\'};
+
+  countSyntaxes('c', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'"', 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
+shouldNotCountClosingDoubleQuoteWhenInEscapeMode(const MunitParameter params[],
+                                                 void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+  char modes[] = {'"', 0, 0, '\\'};
+
+  countSyntaxes('"', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'"', 0, 0, 0}, 4);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
+shouldNotCountClosingSingleQuoteWhenInEscapeMode(const MunitParameter params[],
+                                                 void *fixture) {
+  int syntaxesCount[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char modes[] = {'\'', 0, 0, '\\'};
+
+  countSyntaxes('\'', syntaxesCount, modes);
+
+  assert_arrays_int_equal(syntaxesCount,
+                          (int[]){0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 12);
+  assert_arrays_char_equal(modes, (char[]){'\'', 0, 0, 0}, 4);
 
   return MUNIT_OK;
 }
@@ -480,14 +630,87 @@ MunitTest tests[] = {
         NULL,                                              /* setup */
         NULL,                                              /* tear_down */
         MUNIT_TEST_OPTION_NONE,                            /* options */
-        NULL                                               /* parameters */
+        test_params_double_quotes                          /* parameters */
+    },
+    {
+        "should not try to initialize multi comment mode when there is no "
+        "slash previously", /* name
+                             */
+        shouldNotTryToInitializeMultiCommentModeWhenThereIsNoSlashPreviously, /* test */
+        NULL,                   /* setup */
+        NULL,                   /* tear_down */
+        MUNIT_TEST_OPTION_NONE, /* options */
+        NULL                    /* parameters */
+    },
+    {
+        "should not count char when in single quote mode", /* name
+                                                            */
+        shouldNotCountCharWhenInSingleQuote,               /* test */
+        NULL,                                              /* setup */
+        NULL,                                              /* tear_down */
+        MUNIT_TEST_OPTION_NONE,                            /* options */
+        test_params_single_quotes                          /* parameters */
+    },
+    {
+        "should not count char when in multi comments mode", /* name
+                                                              */
+        shouldNotTryToCountCharWhenInMultipleComments,       /* test */
+        NULL,                                                /* setup */
+        NULL,                                                /* tear_down */
+        MUNIT_TEST_OPTION_NONE,                              /* options */
+        test_params_multi_comments_quotes                    /* parameters */
+    },
+    {
+        "should not count char when in line comment mode", /* name
+                                                            */
+        shouldNotTryToCountCharWhenInLineComment,          /* test */
+        NULL,                                              /* setup */
+        NULL,                                              /* tear_down */
+        MUNIT_TEST_OPTION_NONE,                            /* options */
+        test_params_line_comments_quotes                   /* parameters */
+    },
+    {
+        "should be able to pass in escape mode", /* name
+                                                  */
+        shouldBeAbleToPassInEscapeMode,          /* test */
+        NULL,                                    /* setup */
+        NULL,                                    /* tear_down */
+        MUNIT_TEST_OPTION_NONE,                  /* options */
+        NULL                                     /* parameters */
+    },
+    {
+        "should be able to exit escape mode", /* name
+                                               */
+        shouldBeAbleToExitEscapeMode,         /* test */
+        NULL,                                 /* setup */
+        NULL,                                 /* tear_down */
+        MUNIT_TEST_OPTION_NONE,               /* options */
+        NULL                                  /* parameters */
+    },
+    {
+        "should not count closing double quote when in escape mode", /* name
+                                                                      */
+        shouldNotCountClosingDoubleQuoteWhenInEscapeMode,            /* test */
+        NULL,                                                        /* setup */
+        NULL,                   /* tear_down */
+        MUNIT_TEST_OPTION_NONE, /* options */
+        NULL                    /* parameters */
+    },
+    {
+        "should not count closing single quote when in escape mode", /* name
+                                                                      */
+        shouldNotCountClosingSingleQuoteWhenInEscapeMode,            /* test */
+        NULL,                                                        /* setup */
+        NULL,                   /* tear_down */
+        MUNIT_TEST_OPTION_NONE, /* options */
+        NULL                    /* parameters */
     },
     /* Mark the end of the array with an entry where the test
      * function is NULL */
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}};
 
 static const MunitSuite suite = {
-    "/count-syntaxes ",     /* name */
+    "",                     /* name */
     tests,                  /* tests */
     NULL,                   /* suites */
     1,                      /* iterations */
