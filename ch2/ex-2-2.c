e3773d979d51c23a6497e47cde4b9421a8849138
cgombauld <clement.gombauld@cpexterne.org>
2024-10-06 16:23:43 +0200

    save

--- ch2/ex-2-2.c
+++ ch2/ex-2-2.c
@@ -5,7 +5,7 @@ int main() {
   int c;
   char s[lim];
   int i;
-  for (i = 0; i < lim - 1 ? 0 : (c = getchar()) != '\n' ? 1 : c != EOF; i++) {
+  for (i = 0; i < lim - 1 ? (c = getchar()) != '\n' ? c != EOF : 0 : 0; i++) {
     s[i] = c;
   }
   return 0;
