82364527f28caeca92ac69c12144cdc6f3851f2f
cgombauld <clement.gombauld@cpexterne.org>
2024-10-14 09:17:51 +0200

    solve exercise 1 of chapter 3

---
+++ ch3/ex-3-1.c
@@ -0,0 +1,14 @@
+int binsearch(int x, int v[], int n) {
+  int low, high, mid;
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (x < v[mid]) {
+      high = mid + 1;
+    } else {
+      low = mid + 1;
+    };
+  }
+  return x == v[mid] ? mid : -1;
+}
