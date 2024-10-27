d46f13367078ebaafa6064fff2d3bde2082de5ee
cgombauld <clement.gombauld@cpexterne.org>
2024-10-27 17:06:37 +0100

    started to implment exerise 12 of chapter 4

---
+++ ch4/ex-4-12.c
@@ -0,0 +1,31 @@
+#include <string.h>
+
+void reverse(char s[]) {
+  int c, i, j;
+  for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+/*
+void itoa(int n, char s[]) {
+  int i, sign;
+  unsigned int number;
+  if ((sign = n) < 0) {
+    number = -n;
+  } else {
+    number = n;
+  }
+  i = 0;
+  do {
+    s[i++] = number % 10 + '0';
+  } while ((number /= 10) > 0);
+  if (sign < 0)
+    s[i++] = '-';
+  s[i] = '\0';
+  reverse(s);
+}
+
+*/
