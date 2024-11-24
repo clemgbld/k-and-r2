1cc666af2515a598b9d5762b7726249a2d94f52f
cgombauld <clement.gombauld@cpexterne.org>
2024-11-24 18:17:57 +0100

    solve ex 18 of chapter 5

--- ch5/ex-5-18.c
+++ ch5/ex-5-18.c
@@ -63,8 +63,9 @@ void dirdcl(void) {
       printf("error: missing )\n");
   } else if (tokentype == NAME) /* variable name */
     strcpy(name, token);
-  else
+  else {
     printf("error: expected name or (dcl)\n");
+  }
   while ((type = gettoken()) == PARENS || type == BRACKETS)
     if (type == PARENS)
       strcat(out, " function returning");
