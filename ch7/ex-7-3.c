38a34f48a9d7c2d8e16b7c83e43a161698ffc495
cgombauld <clement.gombauld@cpexterne.org>
2024-12-14 13:51:20 +0100

    started exercise 3 of chapter 7

--- ch7/ex-7-3.c
+++ ch7/ex-7-3.c
@@ -1,8 +1,30 @@
+#include "../ch6/take-first.c"
 #include <_ctype.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void display_padded_str(char *str, bool has_first_int, int first_int) {
+  int str_len = strlen(str);
+  if (has_first_int && first_int > 0) {
+    int pad_start_len = first_int - str_len;
+    for (int i = 0; i < pad_start_len; i++) {
+      putchar(' ');
+    }
+  }
+  for (; *str; str++) {
+    putchar(*str);
+  }
+  if (has_first_int && first_int < 0) {
+    int pad_start_len = abs(first_int) - str_len;
+    for (int i = 0; i < pad_start_len; i++) {
+      putchar(' ');
+    }
+  }
+}
 
 /* minprintf: minimal printf with variable argument list */
 void minprintf(char *fmt, ...) {
@@ -51,16 +73,26 @@ void minprintf(char *fmt, ...) {
 
     if (*p == '.') {
       p++;
+      if (isnumber(*p)) {
+        second_int = 10 * second_int + (*p++ - '0');
+        for (; isnumber(*p); p++) {
+          second_int = 10 * second_int + (*p - '0');
+        }
+        has_second_int = true;
+      } else if (*p == '*') {
+        second_int = va_arg(ap, int);
+        second_int = true;
+        p++;
+      }
     }
 
+    char str[sizeof(long)];
     switch (*p) {
     case 'd':
-      ival = va_arg(ap, int);
-      printf("%d", ival);
-      break;
     case 'i':
       ival = va_arg(ap, int);
-      printf("%d", ival);
+      sprintf(str, "%d", ival);
+      display_padded_str(str, has_first_int, first_int);
       break;
     case 'f':
       dval = va_arg(ap, double);
@@ -68,35 +100,49 @@ void minprintf(char *fmt, ...) {
       break;
     case 'e':
       dval = va_arg(ap, double);
-      printf("%e.ddd", dval);
+      printf("%e", dval);
       break;
+    case 'G':
     case 'g':
       dval = va_arg(ap, double);
       printf("%g", dval);
       break;
     case 'c':
       ival = va_arg(ap, int);
-      printf("%c", ival);
+      sprintf(str, "%c", ival);
+      display_padded_str(str, has_first_int, first_int);
       break;
     case 'u':
       ival = va_arg(ap, int);
-      printf("%u", ival);
+      sprintf(str, "%u", ival);
+      display_padded_str(str, has_first_int, first_int);
       break;
     case 'o':
       ival = va_arg(ap, int);
-      printf("%o", ival);
+      sprintf(str, "%o", ival);
+      display_padded_str(str, has_first_int, first_int);
       break;
     case 'x':
+    case 'X':
       ival = va_arg(ap, int);
-      printf("%x", ival);
+      sprintf(str, "%x", ival);
+      display_padded_str(str, has_first_int, first_int);
       break;
     case 's':
-      for (sval = va_arg(ap, char *); *sval; sval++)
-        putchar(*sval);
+      sval = va_arg(ap, char *);
+      if (has_second_int) {
+        char *final_s;
+        final_s = take_first(sval, second_int);
+        display_padded_str(final_s, has_first_int, first_int);
+        free(final_s);
+      } else {
+        display_padded_str(sval, has_first_int, first_int);
+      }
       break;
     case 'p':
       void_p = va_arg(ap, void *);
-      printf("%p", void_p);
+      sprintf(str, "%p", void_p);
+      display_padded_str(str, has_first_int, first_int);
       break;
     default:
       putchar(*p);
@@ -106,4 +152,4 @@ void minprintf(char *fmt, ...) {
   va_end(ap); /* clean up when done */
 }
 
-int main() { return 0; }
+int main() { minprintf("%x %x %x %x %x\n", 255, 255, 255, 255, 255); }
