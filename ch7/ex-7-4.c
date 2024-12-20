24e8c233a30e01a4f030be3b358b24f398f5a2b3
cgombauld <clement.gombauld@cpexterne.org>
2024-12-20 10:32:22 +0100

    handle float,double and long double number

--- ch7/ex-7-4.c
+++ ch7/ex-7-4.c
@@ -1,5 +1,6 @@
 #include <_ctype.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -36,6 +37,53 @@ void ungetch(int c) /* push character back on input */
     }                                                                          \
   }
 
+#define parse_decimal(c, dval, has_width, width, sign)                         \
+  {                                                                            \
+    if (c == '.') {                                                            \
+      width--;                                                                 \
+      if (width > 1 || !has_width) {                                           \
+        int power = 1;                                                         \
+        for (; isdigit(c = getch());) {                                        \
+          if (has_width && width == 0) {                                       \
+            break;                                                             \
+          }                                                                    \
+          dval = (10.0 * dval) + (c - '0');                                    \
+          power *= 10;                                                         \
+          width--;                                                             \
+        }                                                                      \
+        dval = dval / power;                                                   \
+        if (width > 1 || !has_width) {                                         \
+          if (c == 'e' || c == 'E') {                                          \
+            c = getch();                                                       \
+            width--;                                                           \
+            if (width > 1 || !has_width) {                                     \
+              if (c == '-') {                                                  \
+                sign = -1;                                                     \
+                width--;                                                       \
+              } else if (c == '+') {                                           \
+                sign = 1;                                                      \
+                width--;                                                       \
+              }                                                                \
+              if (width > 1 || !has_width) {                                   \
+                double exp = 0;                                                \
+                for (; isdigit(c = getch());) {                                \
+                  if (has_width && width == 0) {                               \
+                    break;                                                     \
+                  }                                                            \
+                  exp = (10 * exp) + (c - '0');                                \
+                  width--;                                                     \
+                }                                                              \
+                if (exp > 0) {                                                 \
+                  dval = (dval) * (pow(10, (sign * exp)));                     \
+                }                                                              \
+              }                                                                \
+            }                                                                  \
+          }                                                                    \
+        }                                                                      \
+      }                                                                        \
+    }                                                                          \
+  }
+
 int minscanf(char *fmt, ...) {
   va_list ap;        /* points to each unnamed arg in turn */
   va_start(ap, fmt); /* make ap point to 1st unnamed arg */
@@ -55,6 +103,7 @@ int minscanf(char *fmt, ...) {
       int has_width = 0;
       int should_skip = 0;
       int has_l_modifier = 0;
+      int has_L_modifier = 0;
       int has_h_modifier = 0;
       int is_negative = 0;
 
@@ -82,6 +131,9 @@ int minscanf(char *fmt, ...) {
       } else if (*fmt == 'h') {
         fmt++;
         has_h_modifier = 1;
+      } else if (*fmt == 'L') {
+        fmt++;
+        has_L_modifier = 1;
       }
 
       if (*fmt == 'd' || *fmt == 'i') {
@@ -129,15 +181,63 @@ int minscanf(char *fmt, ...) {
       }
 
       if (*fmt == 's') {
-        char *char_p = va_arg(ap, char *);
-        *char_p++ = c;
-        for (int i = 0; !isspace(c = getchar()); i++) {
-          if (has_width && width == i) {
-            ungetch(c);
-            break;
+        if (should_skip) {
+          for (int i = 0; !isspace(c = getchar()); i++) {
+            if (has_width && width == i) {
+              break;
+            }
           }
+        } else {
+          char *char_p = va_arg(ap, char *);
           *char_p++ = c;
+          for (int i = 0; !isspace(c = getchar()); i++) {
+            if (has_width && width == i) {
+              break;
+            }
+            *char_p++ = c;
+          }
         }
+        ungetch(c);
+        continue;
+      }
+
+      if (*fmt == 'e' || *fmt == 'f' || *fmt == 'g') {
+        if (c == '-') {
+          is_negative = 1;
+          c = getch();
+        }
+
+        if (!isnumber(c)) {
+          break;
+        }
+        int sign = is_negative ? -1 : 1;
+
+        if (has_L_modifier) {
+          long double ldval;
+          parse_number(c, ldval, has_width, width);
+          parse_decimal(c, ldval, has_width, width, sign);
+          if (!should_skip) {
+            long double *ld_p = va_arg(ap, long double *);
+            *ld_p = is_negative ? ldval * -1 : ldval;
+          }
+        } else if (has_l_modifier) {
+          double dval;
+          parse_number(c, dval, has_width, width);
+          parse_decimal(c, dval, has_width, width, sign);
+          if (!should_skip) {
+            double *d_p = va_arg(ap, double *);
+            *d_p = is_negative ? dval * -1 : dval;
+          }
+        } else {
+          float fval;
+          parse_number(c, fval, has_width, width);
+          parse_decimal(c, fval, has_width, width, sign);
+          if (!should_skip) {
+            float *f_p = va_arg(ap, float *);
+            *f_p = is_negative ? fval * -1 : fval;
+          }
+        }
+
         ungetch(c);
         continue;
       }
@@ -157,10 +257,8 @@ int minscanf(char *fmt, ...) {
 }
 
 int main() {
-  int num;
-  int num2;
-  char s[100];
-  minscanf("%d %s %d", &num, &s, &num2);
-  printf("%d %s %d\n", num, s, num2);
+  double dval;
+  minscanf("%lf", &dval);
+  printf("%f\n", dval);
   return 0;
 }
